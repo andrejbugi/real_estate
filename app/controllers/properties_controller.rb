a780f7ca233fffe4b6b8064b8f52b3b2cc8da5b7
andrejbugi <andrejbugi96@gmail.com>
2020-12-24 01:40:09 +0100

    Install Active Storage for adding images

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -4,13 +4,57 @@ class PropertiesController < ApplicationController
   end
 
   def show
-    @property = Property.find_by[:params_id]
+    @property = Property.find(params[:id])
+  end
+
+  def new
+    @property = Property.new
+  end
+
+  def create
+    @property = Property.new(property_params)
+
+    if @property.save
+      redirect_to properties_path
+      flash[:success] = 'Property added'
+    else
+      render :new
+      flash[:danger] = 'Cannot add this property'
+    end
+  end
+
+  def edit
+    @property = Porperty.find(params[:id])
+  end
+
+  def update
+    @property = Property.find(params[:id])
+
+    if @property.update
+      redirect_to properties_path
+      flash[:success] = 'Successfully updated property'
+    else
+      render :edit
+      flash[:danger] = 'Cannot edit this property'
+    end
+  end
+
+  def destroy
+    @property = Property.find(params[:id])
+
+    if @property.delete
+      redirect_to properties_path
+      flash[:success] = 'Successfully deleted this property'
+    else
+      redirect_to @property
+      flash[:danger] = 'Cannot delete this property'
+    end
   end
 
   private
 
   def property_params
     params.require(:property).permit(:title, :description, :category, :bedroom, :bathroom,
-                                     :location, :price, :listing_type, :interior)
+                                     :location, :price, :listing_type, :interior, images: [])
   end
 end
--- app/models/property.rb
+++ app/models/property.rb
@@ -1,11 +1,13 @@
 class Property < ApplicationRecord
-  validates :title, presence: true
-  validates :description, presence: true
+  validates :title, presence: true, length: { maximum: 20 }
+  validates :description, presence: true, length: { minimum: 20, maximum: 256 }
   validates :category, presence: true
-  validates :bedroom, presence: true
-  validates :bathroom, presence: true
+  validates :bedroom, presence: true, numericality: { only_integer: true }
+  validates :bathroom, presence: true, numericality: { only_integer: true }
   validates :location, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
   validates :listing_type, presence: true
   validates :interior, presence: true
+
+  has_many_attached :images
 end
